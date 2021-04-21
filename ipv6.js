da764ad8c421a58fdbc3796c760bb5afcdd8edd9
reyalvarado <reyalvarado@yahoo.com>
2021-04-21 22:57:32 +0000

    Lab 4

--- ipv6.js
+++ ipv6.js
@@ -1,3 +1,5 @@
+module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
+
 /**
  * Calculates an IPv4-mapped IPv6 address.
  * @param {string} ipv4 - An IPv4 address in dotted-quad format.
@@ -47,4 +49,4 @@ function getIpv4MappedIpv6Address(ipv4) {
   return ipv6Address;
 }
 
-module.exports.getIpv4MappedIpv6Address = getIpv4MappedIpv6Address;
+
